4445ddbf0d961d15a02cf7b7b464f9b928d7ab6b
Hanan Milman <hananmilman@gmail.com>
2022-08-08 18:54:55 +0300

    prometheus connected

--- load-stub-rest-dotnet/Program.cs
+++ load-stub-rest-dotnet/Program.cs
@@ -1,5 +1,9 @@
 using Load.Stub.Rest.dotNet.Effects;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
+using Prometheus;
+using Prometheus.DotNetRuntime;
 using Swashbuckle.AspNetCore.Filters;
 using System.Diagnostics;
 using System.Reflection;
@@ -13,12 +17,19 @@ builder.Services.AddTransient<IOperationEffect, CpuConsumeOperation>();
 builder.Services.AddTransient<EffectsProvider>();
 builder.Services.AddTransient<IApplyEffectsModifier, ApplyEffectModifier>();
 
-builder.Services.AddTransient<IApplySpecificEffectModifier, FixedValueEffectModifier >();
+builder.Services.AddTransient<IApplySpecificEffectModifier, FixedValueEffectModifier>();
 builder.Services.AddTransient<IApplySpecificEffectModifier, MultiplierEffectModifier>();
 builder.Services.AddTransient<IApplySpecificEffectModifier, AdditionEffectModifier>();
 builder.Services.AddTransient<IApplySpecificEffectModifier, RandomRangeEffectModifier>();
 builder.Services.AddTransient<IApplySpecificEffectModifier, SinWaveEffectModifier>();
 
+var metrics = new KestrelMetricServer(port: 9090).Start();
+var diagnostocPrometheus = DiagnosticSourceAdapter.StartListening();
+var evenyCounterAdapter = EventCounterAdapter.StartListening();
+
+IDisposable statsCollector = DotNetRuntimeStatsBuilder.Default().StartCollecting();
+
+builder.Services.AddHealthChecks();
 
 builder.Services.AddOpenTracing(otb =>
 {
@@ -50,28 +61,62 @@ builder.Services.AddSwaggerGen((opt) =>
         opt.UseAllOfForInheritance();
         opt.UseAllOfToExtendReferenceSchemas();
         opt.UseOneOfForPolymorphism();
-        
+
         var xmlFilename = $"{assembly.GetName().Name}.xml";
         opt.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
     });
 
-builder.Services.AddTransient<IHostedService, MyHostedService>();
 
+builder.Services.AddHttpClient(Options.DefaultName)
+        .UseHttpClientMetrics();
 
+builder.Services.AddHttpLogging(g =>
+{
+
+});
+
+builder.Host.ConfigureHostOptions(ho =>
+{
+    ho.ShutdownTimeout = TimeSpan.FromSeconds(20);
+    ho.BackgroundServiceExceptionBehavior = BackgroundServiceExceptionBehavior.Ignore;
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseHealthChecks("/health");
 
-app.UseAuthorization();
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+//HttpClientMetricsExtensions.UseHttpClientMetrics(app);
+app.UseMetricServer();
+app.UseHttpMetrics();
+app.MapMetrics("/metrics");
+app.UseRouting();
+app.UseEndpoints(
+    endpoints =>
+    {
+        endpoints.MapControllers();
+        endpoints.MapMetrics();
+    }
+);
 
 app.MapControllers();
- 
-await app.RunAsync();
 
+try
+{
+    await app.RunAsync();
+}
+finally
+{
+    // Stops listening for DiagnosticSource events.
+    diagnostocPrometheus.Dispose();
+    // Stops listening for EventCounter events.
+    evenyCounterAdapter.Dispose();
 
+    statsCollector.Dispose();
+
+    await metrics.StopAsync();
+}
